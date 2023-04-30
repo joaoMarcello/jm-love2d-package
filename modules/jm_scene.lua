bca65615c4be935935187f806457e18dcd256a72
Joao Moreira <joaomarcello.mm@gmail.com>
2023-04-30 11:40:36 -0300

    changed dt for transition update

--- modules/jm_scene.lua
+++ modules/jm_scene.lua
@@ -702,6 +702,7 @@ local update = function(self, dt)
     end
 
     if self.transition then
+        local dt = dt > (1 / 60) and (1 / 60) or dt
         self.transition:__update__(dt)
         local r = self.trans_action and self.trans_action(dt)
 
