472a4e42a511cb7a2922180401d24945fe667dca
Joao Moreira <joaomarcello.mm@gmail.com>
2023-05-31 10:26:57 -0300

    inseri template para game_object e body_object

---
+++ modules/templates/game_object.lua
@@ -0,0 +1,40 @@
+local GC = _G.Pack.GameObject
+
+---@class GenericObject : GameObject
+local Object = setmetatable({}, GC)
+Object.__index = Object
+
+function Object:new(x, y, w, h, draw_order, update_order, reuse_tab)
+    local obj = GC:new(x, y, w, h, draw_order, update_order, reuse_tab)
+    setmetatable(obj, self)
+    Object.__constructor__(obj)
+    return obj
+end
+
+function Object:__constructor__()
+    --
+    self.update = Object.update
+    self.draw = Object.draw
+end
+
+function Object:load()
+
+end
+
+function Object:init()
+
+end
+
+function Object:update(dt)
+
+end
+
+function Object:my_draw()
+
+end
+
+function Object:draw()
+    GC.draw(self, self.my_draw)
+end
+
+return Object
