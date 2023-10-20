3d771ea2c3b3b43bd6cbeb623567c22af4cb8f26
Joao Moreira <joaomarcello.mm@gmail.com>
2023-10-20 10:57:05 -0300

    added switch_keyboard_owner

--- modules/jm_controller_manager.lua
+++ modules/jm_controller_manager.lua
@@ -9,9 +9,22 @@ local Manager = {
     joy_to_controller = {},
 }
 Manager.keyboard_owner = Manager.P1
-Manager.P2.is_keyboard_owner = false
+Manager.P2.is_keyboard_owner = true
 Manager[1] = Manager.P1
 Manager[2] = Manager.P2
 
+---@param controller JM.Controller
+function Manager:switch_keyboard_owner(controller)
+    if not controller then return end
+
+    for i = 1, self.n do
+        ---@type JM.Controller
+        local c = self[i]
+        c.is_keyboard_owner = false
+    end
+
+    self.keyboard_owner = controller
+    controller.is_keyboard_owner = true
+end
 
 return Manager
