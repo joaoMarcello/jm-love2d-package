20342411b764cb8afce49213641c462649ef7339
Joao Moreira <joaomarcello.mm@gmail.com>
2023-11-14 19:36:39 -0300

    fix print / remove recursion in print / fix default format when not set bold or italic

--- modules/jm_font_generator.lua
+++ modules/jm_font_generator.lua
@@ -240,7 +240,8 @@ function Font:__constructor__(args)
     )
 
 
-    if not args.regular_data or args.bold_data then
+    -- if not args.regular_data or args.bold_data then
+    if args.bold_data or args.dir_bold then
         self:load_glyphs(args.bold_data
             or args.dir_bold,
             -- or str_format(dir, args.name, args.name .. "_bold"),
@@ -255,7 +256,8 @@ function Font:__constructor__(args)
         self.__imgs[FontFormat.bold] = self.__imgs[FontFormat.normal]
     end
 
-    if (not args.regular_data or args.italic_data) then
+    -- if (not args.regular_data or args.italic_data) then
+    if (args.italic_data or args.dir_italic) then
         self:load_glyphs(args.italic_data
             or args.dir_italic,
             -- or str_format(dir, args.name, args.name .. "_italic"),
@@ -1272,91 +1274,140 @@ function Font:print(text, x, y, w, h, __i__, __color__, __x_origin__, __format__
     if not codes then
         codes = {}
         for p, c in utf8.codes(text) do
-            table.insert(codes, utf8.char(c))
+            tab_insert(codes, utf8.char(c))
         end
         codes_result[text] = codes
     end
 
-    -- while (i <= text_size) do
     while i <= #codes do
         ---
-        local glyph_id = codes[i] --text:sub(i, i)
-        local is_a_nick = self:__is_a_nickname(text, i)
+        local glyph_id = codes[i]
+        local pos = utf8.offset(text, i)
+
+        local is_a_nick = self:__is_a_nickname(text, pos)
 
         if is_a_nick then
             glyph_id = is_a_nick
             i = i + #glyph_id - 1
         end
 
-        local tag = text:match("<.->", i)
-        if tag then
-            local match = self:__is_a_command_tag(tag)
+        -- local tag = text:match("<.->", pos)
+        -- if tag then
+        --     local match = self:__is_a_command_tag(tag)
 
-            local startp, endp = text:find("<.->", i)
+        --     local startp, endp
+        --     if match then
+        --         startp, endp = text:find("<.->", pos)
+        --     end
 
-            local r_tx, r_ty
+        --     local r_tx, r_ty
+        --     if match then
+        --         r_tx, r_ty = self:print(text:sub(pos, startp - 1),
+        --             tx, ty, w, h, 1,
+        --             cur_color, x_origin, cur_format, cur_fontsize
+        --         )
+        --     end
+
+        --     if match == "<color>" then
+        --         local parse = Utils:parse_csv_line(text:sub(startp - 1, endp - 1))
+        --         local r = parse[2] or 1
+        --         local g = parse[3] or 0
+        --         local b = parse[4] or 0
+        --         local a = parse[5] or 1
+
+        --         cur_color = Utils:get_rgba(r, g, b, a)
+        --         --
+        --     elseif match == "<font>" then
+        --         local tag_values = match and self:get_tag_args(tag)
+        --         local action = tag_values["font"]
+
+        --         if action == "color-hex" then
+        --             local r, g, b, a =
+        --                 Utils:hex_to_rgba_float(tag_values["value"])
+        --             cur_color = Utils:get_rgba(r, g, b, a)
+        --             ---
+        --         elseif action == "font-size" then
+        --             self:set_font_size(tag_values["value"] or original_fontsize)
+        --         end
+
+        --         ---
+        --     elseif match == "</color>" then
+        --         cur_color = original_color
+        --     elseif match == "<bold>" then
+        --         cur_format = self.format_options.bold
+        --     elseif match == "</bold>" then
+        --         cur_format = original_format
+        --     elseif match == "<italic>" then
+        --         cur_format = self.format_options.italic
+        --     elseif match == "</italic>" then
+        --         cur_format = original_format
+        --     end
+
+        --     if match then
+        --         i = endp - 1
+        --         -- i = i + startp
+
+        --         if endp == #text then
+        --             -- i = i + 1
+        --         end
+        --         tx = r_tx
+        --         ty = r_ty
+        --         glyph_id = ""
+        --     end
+        -- end
+
+        if glyph_id == "<" then
+            local match = self:__is_a_command_tag(text:sub(pos))
             if match then
-                r_tx, r_ty = self:print(text:sub(i, startp - 1),
-                    tx, ty, w, h, 1,
-                    cur_color, x_origin, cur_format, cur_fontsize
-                )
-            end
+                local startp, endp = text:find("<.->", pos)
 
+                ---@diagnostic disable-next-line: param-type-mismatch
+                local tag = text:sub(startp, endp)
 
-            if match == "<color>" then
-                local parse = Utils:parse_csv_line(text:sub(startp - 1, endp - 1))
-                local r = parse[2] or 1
-                local g = parse[3] or 0
-                local b = parse[4] or 0
-                local a = parse[5] or 1
+                if match == "<color>" then
+                    local parse = Utils:parse_csv_line(text:sub(startp - 1, endp - 1))
+                    local r = parse[2] or 1
+                    local g = parse[3] or 0
+                    local b = parse[4] or 0
+                    local a = parse[5] or 1
 
-                cur_color = Utils:get_rgba(r, g, b, a)
-                --
-            elseif match == "<font>" then
-                local tag_values = match and self:get_tag_args(tag)
-                local action = tag_values["font"]
-
-                if action == "color-hex" then
-                    local r, g, b, a =
-                        Utils:hex_to_rgba_float(tag_values["value"])
                     cur_color = Utils:get_rgba(r, g, b, a)
+                    --
+                elseif match == "<font>" then
+                    local tag_values = match and self:get_tag_args(tag)
+                    local action = tag_values["font"]
+
+                    if action == "color-hex" then
+                        local r, g, b, a =
+                            Utils:hex_to_rgba_float(tag_values["value"])
+                        cur_color = Utils:get_rgba(r, g, b, a)
+                        ---
+                    elseif action == "font-size" then
+                        self:set_font_size(tag_values["value"] or original_fontsize)
+                    end
+
                     ---
-                elseif action == "font-size" then
-                    self:set_font_size(tag_values["value"] or original_fontsize)
+                elseif match == "</color>" then
+                    cur_color = original_color
+                elseif match == "<bold>" then
+                    cur_format = self.format_options.bold
+                elseif match == "</bold>" then
+                    cur_format = original_format
+                elseif match == "<italic>" then
+                    cur_format = self.format_options.italic
+                elseif match == "</italic>" then
+                    cur_format = original_format
                 end
 
-                ---
-            elseif match == "</color>" then
-                cur_color = original_color
-            elseif match == "<bold>" then
-                cur_format = self.format_options.bold
-            elseif match == "</bold>" then
-                cur_format = original_format
-            elseif match == "<italic>" then
-                cur_format = self.format_options.italic
-            elseif match == "</italic>" then
-                cur_format = original_format
-            end
-
-            if match then
-                i = endp
-                if endp == #text then
-                    i = i + 1
-                end
-                tx = r_tx
-                ty = r_ty
-                glyph_id = ""
+                glyph_id = nil
+                local off = utf8.offset(text, i) - i
+                i = endp - off
             end
         end
 
         self:set_format_mode(cur_format)
 
-        local glyph = self:__get_char_equals(glyph_id)
-
-        -- if not glyph then
-        --     glyph = self:__get_char_equals(text:sub(i, i + 1))
-        --     if glyph then i = i + 1 end
-        -- end
+        local glyph = glyph_id and self:__get_char_equals(glyph_id)
 
         if glyph_id == "\n"
             or ((glyph and w)
@@ -1434,6 +1485,7 @@ function Font:print(text, x, y, w, h, __i__, __color__, __x_origin__, __format__
     for _, batch in pairs(self.batches) do
         if batch:getCount() > 0 then love_draw(batch) end
     end
+
     self:pop()
     return tx, ty
 end
