e9bfd4893ef5bc33cc9b4d16b93534856f3ddd40
Joao Moreira <joaomarcello.mm@gmail.com>
2023-12-23 21:41:57 -0300

    rounding camera position on update

--- modules/cam_controllers/controller.lua
+++ modules/cam_controllers/controller.lua
@@ -682,6 +682,9 @@ function Controller:update(dt)
         update_on_deadzone(self, dt)
     end
 
+    self.camera.x = Utils:round(self.camera.x)
+    self.camera.y = Utils:round(self.camera.y)
+
     self.camera:keep_on_bounds()
 end
 
