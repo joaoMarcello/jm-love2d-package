4f83c52865a6fe081ff1dd2d4b4a7e519567d1e0
Joao Moreira <joaomarcello.mm@gmail.com>
2023-12-31 11:58:26 -0300

    fix code for distance of colors

--- data/shader/pico8.glsl
+++ data/shader/pico8.glsl
@@ -15,10 +15,40 @@ vec3(41.0/255.0, 173.0/255.0, 1.0),
 vec3(131.0/255.0, 118.0/255.0, 156.0/255.0),
 vec3(1.0, 119.0/255.0, 168.0/255.0),
 vec3(1.0, 204.0/255.0, 170.0/255.0));
+
 vec4 effect(vec4 color, Image tex, vec2 tc, vec2 coord)
 {
     vec4 pix = Texel(tex, tc);
-    float avg = min(0.9999, max(0.0001, (pix.r + pix.g + pix.b)/3.0));
-    int index = int(avg*16.0);
+    int index = 0;
+    float min_dist = 100000.0;
+
+    for(int i=0; i < 16; i++){
+        // float dist = distance(pix, vec4(palette[i],1.0));
+
+        vec3 distVec = (pix.rgb) - palette[i];
+        float R = 0.5 * (pix.r + palette[i].r);
+
+        float dist;
+        if(R < 0.5019607843){ // (128 / 255)
+            dist = sqrt(2.0 * pow(distVec.r, 2.0)
+                + 4.0 * pow(distVec.g, 2.0)
+                + 3.0 * pow(distVec.b, 2.0)
+            );
+        }
+        else{
+            dist = sqrt(3.0 * pow(distVec.r, 2.0)
+                + 4.0 * pow(distVec.g, 2.0)
+                + 2.0 * pow(distVec.b, 2.0)
+            );
+        }
+
+        // dist = sqrt(dot(distVec, distVec));
+
+        if (dist < min_dist){
+            min_dist = dist;
+            index = i;
+        }
+    }
+    
     return vec4(palette[index], pix.a);
 }
