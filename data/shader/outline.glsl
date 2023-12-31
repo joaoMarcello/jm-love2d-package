1e6647eed8a634456a5c9508aa63a3c3a04f6634
Joao Moreira <joaomarcello.mm@gmail.com>
2023-12-31 12:25:18 -0300

    modify to accept extern color

--- data/shader/outline.glsl
+++ data/shader/outline.glsl
@@ -1,18 +1,18 @@
 // https://love2d.org/forums/viewtopic.php?t=85284&sid=dbbc45ad7847be2605acfb59e6f24698
-extern vec2 stepSize = vec2(1.0, 1.0);
-vec4 resultCol;
 
-vec4 effect( vec4 col, Image texture, vec2 texturePos, vec2 screenPos )
+//https://blogs.love2d.org/content/let-it-glow-dynamically-adding-outlines-characters
+extern vec2 stepSize = vec2(0.025, 0.025);
+extern vec3 iColor = vec3(0.4, 1.0, 0.1);
+
+vec4 effect( vec4 col, Image tex, vec2 uv, vec2 sc)
 {
 	// get color of pixels:
-	number alpha = -4.0*texture2D( texture, texturePos ).a;
-	alpha += texture2D( texture, texturePos + vec2( stepSize.x, 0.0 ) ).a;
-	alpha += texture2D( texture, texturePos + vec2( -stepSize.x, 0.0 ) ).a;
-	alpha += texture2D( texture, texturePos + vec2( 0.0, stepSize.y ) ).a;
-	alpha += texture2D( texture, texturePos + vec2( 0.0, -stepSize.y ) ).a;
+	float alpha = -4.0*Texel( tex, uv ).a;
+	alpha += Texel( tex, uv + vec2( stepSize.x, 0.0 ) ).a;
+	alpha += Texel( tex, uv + vec2( -stepSize.x, 0.0 ) ).a;
+	alpha += Texel( tex, uv + vec2( 0.0, stepSize.y ) ).a;
+	alpha += Texel( tex, uv + vec2( 0.0, -stepSize.y ) ).a;
 
 	// calculate resulting color
-	resultCol = vec4( 0.4, 1.0, 0.1, alpha );
-	// return color for current pixel
-	return resultCol;
+	return vec4( iColor.rgb, alpha );
 }
