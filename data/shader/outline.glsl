755ae2e3d2046d46fb46f667e238767219df0e04
Joao Moreira <joaomarcello.mm@gmail.com>
2024-01-01 12:47:38 -0300

    remove extern and add uniform

--- data/shader/outline.glsl
+++ data/shader/outline.glsl
@@ -1,8 +1,8 @@
 // https://love2d.org/forums/viewtopic.php?t=85284&sid=dbbc45ad7847be2605acfb59e6f24698
 
 //https://blogs.love2d.org/content/let-it-glow-dynamically-adding-outlines-characters
-extern vec2 stepSize = vec2(0.025, 0.025);
-extern vec3 iColor = vec3(0.4, 1.0, 0.1);
+uniform vec2 stepSize = vec2(0.025, 0.025);
+uniform vec3 iColor = vec3(0.4, 1.0, 0.1);
 
 vec4 effect( vec4 col, Image tex, vec2 uv, vec2 sc)
 {
