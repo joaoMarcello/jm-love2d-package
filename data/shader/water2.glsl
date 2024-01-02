3ca6f5391ed7226b81c5c9e31ed0dc6dbc8c4241
Joao Moreira <joaomarcello.mm@gmail.com>
2024-01-02 09:44:54 -0300

    add direction and speed uniform var

--- data/shader/water2.glsl
+++ data/shader/water2.glsl
@@ -1,16 +1,18 @@
 // https://www.shadertoy.com/view/ldXGz7
 
 uniform float iTime = 0.0;
+uniform vec2 direction = vec2(1.0, 1.0);
+uniform float speed = 3.0;
 
 vec4 effect(vec4 color, Image tex, vec2 uv, vec2 sc)
 {
-    vec2 scaling = vec2(512/256, 288/224);
+	float time = iTime * speed;
 
-	uv.y += (cos((uv.y + (iTime * 0.04)) * 45.0) * 0.0019) +
-		(cos((uv.y + (iTime * 0.1)) * 10.0) * 0.002);
+	uv.y += ((cos((uv.y + (time * 0.04)) * 45.0) * 0.0019) +
+		(cos((uv.y + (time * 0.1)) * 10.0) * 0.002)) * direction.y;
 
-	uv.x += (sin((uv.y + (iTime * 0.07)) * 15.0) * 0.0029) +
-		(sin((uv.y + (iTime * 0.1)) * 15.0) * 0.002);
+	uv.x += ((sin((uv.y + (time * 0.07)) * 15.0) * 0.0029) +
+		(sin((uv.y + (time * 0.1)) * 15.0) * 0.002))*direction.x;
 
 	return Texel(tex,uv);
 }
