7efc3fc3f13f743fa3fd9fcbef0641ba0672eca2
Joao Moreira <joaomarcello.mm@gmail.com>
2024-01-03 13:15:48 -0300

    add smoothstep function

--- modules/jm_utils.lua
+++ modules/jm_utils.lua
@@ -469,6 +469,11 @@ Utils.Domain = Domain
 
 --=========================================================================
 
+function Utils:smoothstep(edge0, edge1, x)
+    local t = Utils:clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+    return t * t * (3.0 - 2.0 * t);
+end
+
 Utils.E = E
 Utils.tanh = tanh
 Utils.sigmoid = sigmoid
