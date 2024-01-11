953aadbebe0ac7ede57a5607456450f7263160f9
Joao Moreira <joaomarcello.mm@gmail.com>
2024-01-11 10:01:54 -0300

    rename texture to tex

--- data/shader/boxblur.glsl
+++ data/shader/boxblur.glsl
@@ -1,10 +1,10 @@
 extern vec2 direction;
 extern number radius;
-vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _) {
+vec4 effect(vec4 color, Image tex, vec2 tc, vec2 _) {
     vec4 c = vec4(0.0);
 
     for (float i = -radius; i <= radius; i += 1.0){
-        c += Texel(texture, tc + i * direction);
+        c += Texel(tex, tc + i * direction);
     }
     return c / (2.0 * radius + 1.0) * color;
 }
