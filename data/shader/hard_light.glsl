adb28292f23f75f4ab9a1c973c9eaa8576df3a07
Joao Moreira <joaomarcello.mm@gmail.com>
2024-01-24 23:01:29 -0300

    add hard_light blend mode shader

---
+++ data/shader/hard_light.glsl
@@ -0,0 +1,26 @@
+uniform vec4 c = vec4(242/255.0, 133.0/255.0, 197.0/255.0, 1.0);
+
+float less_than(float x, float y) {
+    return max(sign(y - x), 0.0);
+}
+
+float is_equal(float x, float y) {
+    return 1.0 - abs(sign(x - y));
+}
+
+float or(float x, float y){
+    return min(x + y, 1.0);
+}
+
+float overlay(float a, float b){
+    float r = less_than(a, 0.5);
+    float r2 = or(less_than(0.5, a), is_equal(a, 0.5));
+
+    return (2.0 * a * b) * r 
+        + (1.0 - 2.0 * (1.0 - a) * (1.0 - b)) * r2;
+}
+
+vec4 effect(vec4 color, Image tex, vec2 uv, vec2 sc){
+    vec4 pix = Texel(tex, uv);
+    return mix(vec4(overlay(c.r, pix.r), overlay(c.g, pix.g), overlay(c.b, pix.b), pix.a), pix, c.a);
+}
