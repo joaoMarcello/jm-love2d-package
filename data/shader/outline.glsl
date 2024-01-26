80f896cfc56acb0e7e9d7d40f37693bc1d01ae07
Joao Moreira <joaomarcello.mm@gmail.com>
2024-01-26 10:25:52 -0300

    changed extern var names

--- data/shader/outline.glsl
+++ data/shader/outline.glsl
@@ -2,17 +2,17 @@
 
 //https://blogs.love2d.org/content/let-it-glow-dynamically-adding-outlines-characters
 uniform vec2 stepSize = vec2(0.025, 0.025);
-uniform vec3 iColor = vec3(0.4, 1.0, 0.1);
+uniform vec3 color = vec3(0.4, 1.0, 0.1);
 
-vec4 effect( vec4 col, Image tex, vec2 uv, vec2 sc)
+vec4 effect(vec4 c, Image tex, vec2 uv, vec2 sc)
 {
 	// get color of pixels:
-	float alpha = -4.0*Texel( tex, uv ).a;
-	alpha += Texel( tex, uv + vec2( stepSize.x, 0.0 ) ).a;
-	alpha += Texel( tex, uv + vec2( -stepSize.x, 0.0 ) ).a;
-	alpha += Texel( tex, uv + vec2( 0.0, stepSize.y ) ).a;
-	alpha += Texel( tex, uv + vec2( 0.0, -stepSize.y ) ).a;
+	float alpha = -4.0 * Texel(tex, uv).a;
+	alpha += Texel(tex, uv + vec2(stepSize.x, 0.0)).a;
+	alpha += Texel(tex, uv + vec2(-stepSize.x, 0.0)).a;
+	alpha += Texel(tex, uv + vec2(0.0, stepSize.y)).a;
+	alpha += Texel(tex, uv + vec2(0.0, -stepSize.y)).a;
 
 	// calculate resulting color
-	return vec4( iColor.rgb, alpha );
+	return vec4( color.rgb, alpha );
 }
