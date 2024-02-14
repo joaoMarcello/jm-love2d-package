4213144f7172335f7ef48b383d08b8e4d0e58212
Joao Moreira <joaomarcello.mm@gmail.com>
2024-02-14 16:25:20 -0300

    calling scene:update after setting scene as the actual state

--- modules/jm_scene_manager.lua
+++ modules/jm_scene_manager.lua
@@ -71,10 +71,9 @@ function Manager:change_gamestate(new_state, conf)
     r = conf.transition and scene:add_transition(conf.transition, "in", conf.transition_conf, nil, conf.trans_end_action) or
         nil
 
+    self.scene = scene
     scene:resize(love.graphics.getDimensions())
     scene:update(love.timer.getDelta())
-
-    self.scene = scene
 
     return r
 end
