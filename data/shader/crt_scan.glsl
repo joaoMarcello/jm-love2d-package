a8f493a8e2e1dbf4c7920da39d9b17b3a6642432
Joao Moreira <joaomarcello.mm@gmail.com>
2024-03-12 16:36:41 -0300

    add uNoise uniform

--- data/shader/crt_scan.glsl
+++ data/shader/crt_scan.glsl
@@ -11,13 +11,29 @@ uniform float opacity = 0.15;
 uniform vec3 scan_color = vec3(0.0, 0.0, 0.0);
 uniform float screen_h;
 
-vec4 scanln(vec4 c,Image tex,vec2 tc){
+// noise
+uniform vec2 uNoise = vec2(0.2, 1.0);
+uniform float uSeed = 0.0;
+
+vec4 scanln(vec4 c, vec2 tc){
     number v = 0.5 * (sin(tc.y * 3.14159 / width * screen_h + phase) + 1.0);
     c.rgb -= (scan_color - c.rgb) * (pow(v, thickness) -1.0) * opacity;
     return c;
 }
 
-vec4 crt(vec4 c, Image tex, vec2 uv, vec2 sc){
+float rand(vec2 co) {
+    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float noise(vec2 coord, vec2 uInputSize)
+{
+    vec2 pixelCoord = coord * uInputSize.xy;
+    pixelCoord.x = floor(pixelCoord.x / uNoise[1]);
+    pixelCoord.y = floor(pixelCoord.y / uNoise[1]);
+    return (rand(pixelCoord * uNoise[1] * uSeed) - 0.5) * uNoise[0];
+}
+
+vec4 crt(vec4 c, vec2 uv){
     uv = (uv * 2.0) - vec2(1.0);
 
     uv *= scaleFactor;
@@ -28,9 +44,19 @@ vec4 crt(vec4 c, Image tex, vec2 uv, vec2 sc){
 
     uv = (uv + vec2(1.0)) / 2.0;
 
-    return c * Texel(tex, uv) * mask;
+    return c * mask;
 }
 
-vec4 effect(vec4 c,Image tex,vec2 uv,vec2 sc){
-    return crt(scanln(c, tex, uv), tex, uv, sc);
+vec4 effect(vec4 c, Image tex, vec2 uv, vec2 sc){
+    vec4 final = Texel(tex, uv);
+
+    if(uNoise[0] > 0.0 && uNoise[1] > 0.0)
+    {
+        float n = noise(uv, love_ScreenSize.xy);
+        final += vec4(n, n, n, final.a);
+    }
+
+    final = scanln(final, uv);
+    final = crt(final, uv);
+    return final;
 }
