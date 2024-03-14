bcb82fd1ac679acfdfbcc74da6760c2b97941bd9
Joao Moreira <joaomarcello.mm@gmail.com>
2024-03-14 17:43:15 -0300

    add buttons start and select

--- modules/jm_virtual_pad.lua
+++ modules/jm_virtual_pad.lua
@@ -6,45 +6,80 @@ local VirtualStick = GUI.VirtualStick
 
 local width, height = love.graphics.getDimensions()
 
-local size = math.floor(height / 5)
+local size = math.floor(height * 0.2)
 
 --==========================================================================
 local Bt_A = TouchButton:new {
     x = 32,
     y = 32,
-    w = size,
-    h = size,
+    w = 128,
+    h = 128,
     use_radius = true,
     text = "A",
     opacity = 0.5,
     on_focus = true,
 }
-Bt_A:set_position(width - 30 - size, height - 30 - size)
-
 --==========================================================================
 local Bt_B = TouchButton:new {
     x = 32,
     y = 32,
-    w = size,
-    h = size,
+    w = 128,
+    h = 128,
     use_radius = true,
     text = "B",
     opacity = 0.5,
     on_focus = true,
 }
-Bt_B:set_position(Bt_A.x - size - 20, Bt_A.y - size * 0.5 - 20)
+--==========================================================================
+local rect_rx = 20
 
+---@param self JM.GUI.TouchButton
+local bt_draw = function(self)
+    local lgx = love.graphics
+    local color = self.color
+    lgx.setColor(color)
+    lgx.rectangle("line", self.x, self.y, self.w, self.h, rect_rx, rect_rx)
+
+    local font = self:get_font()
+    font:push()
+    font:set_color(color)
+    font:set_font_size(self.h * 0.4)
+    font:printf((self.text):upper(), self.x - 20, self.y + self.h * 0.5 - font.__font_size * 0.5, self.w + 40, "center")
+    font:pop()
+end
+
+local Bt_Start = TouchButton:new {
+    x = 32,
+    y = 32,
+    w = 128,
+    h = 128,
+    text = "start",
+    opacity = 0.5,
+    on_focus = true,
+    draw = bt_draw,
+}
+
+local Bt_Select = TouchButton:new {
+    x = 32,
+    y = 32,
+    w = 128,
+    h = 128,
+    text = "select",
+    opacity = 0.5,
+    on_focus = true,
+    draw = bt_draw,
+}
 --==========================================================================
 local stick = VirtualStick:new {
     on_focus = true,
     w = height / 4,
     is_mobile = true,
     bound_top = height * 0.25,
-    bound_width = 1366 / 4,
+    bound_width = width / 4,
     bound_height = height * 0.75,
     opacity = 0.5,
 }
-stick:set_position(stick.max_dist, height - stick.h - 50, true)
+stick:set_position(stick.max_dist, height - stick.h - 130, true)
 --==========================================================================
 
 
@@ -56,7 +91,11 @@ local Pad = {
     [2] = Bt_B,
     Stick = stick,
     [3] = stick,
-    N = 3
+    Start = Bt_Start,
+    [4] = Bt_Start,
+    Select = Bt_Select,
+    [5] = Bt_Select,
+    N = 5
 }
 
 function Pad:mousepressed(x, y, button, istouch, presses)
@@ -83,6 +122,45 @@ function Pad:touchreleased(id, x, y, dx, dy, pressure)
     end
 end
 
+function Pad:set_button_size(value)
+    value = value or (math.min(love.graphics.getDimensions()) * 0.2)
+    Bt_A:set_dimensions(value, value)
+    Bt_A:init()
+    Bt_B:set_dimensions(value, value)
+    Bt_B:init()
+end
+
+function Pad:fix_positions()
+    local w, h = love.graphics.getDimensions()
+    local min, max = math.min(w, h), math.max(w, h)
+    local border_w = w * 0.03
+    local space = 20
+
+    Bt_A:set_position(w - border_w - Bt_A.w, h - border_w - Bt_A.h)
+    Bt_B:set_position(Bt_A.x - Bt_B.w - space, Bt_A.y - Bt_B.h * 0.5)
+
+    do
+        local size = w * 0.1
+        local space = space * 0.5
+        rect_rx = (20 * size) / (975 * 0.1)
+
+        Bt_Start:set_dimensions(size, size * 0.4)
+        Bt_Select:set_dimensions(size, size * 0.4)
+
+        Bt_Start:set_position(
+            w * 0.5 - space - Bt_Start.w,
+            h - (space * 2) - Bt_Start.h
+        )
+
+        Bt_Select:set_position(w * 0.5 + space, Bt_Start.y)
+    end
+end
+
+function Pad:resize()
+    self:set_button_size()
+    self:fix_positions()
+end
+
 function Pad:update(dt)
     for i = 1, self.N do
         self[i]:update(dt)
@@ -94,5 +172,8 @@ function Pad:draw()
         self[i]:draw()
     end
 end
+
+Pad:set_button_size()
+Pad:fix_positions()
 
 return Pad
