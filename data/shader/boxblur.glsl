ca4e60ce3d21132d6bf865c5a33e23ab10a43d28
Joao Moreira <joaomarcello.mm@gmail.com>
2024-03-15 20:04:49 -0300

    fix error when on web, but remove radius usage

--- data/shader/boxblur.glsl
+++ data/shader/boxblur.glsl
@@ -1,9 +1,8 @@
 extern vec2 direction;
 extern float radius;
 vec4 effect(vec4 color, Image tex, vec2 tc, vec2 _) {
-    vec4 c = vec4(0.0);
-
-    for (float i = -radius; i <= radius; i += 1.0){
+    vec4 c = vec4(0.0, 0.0, 0.0, 0.0);
+    for (float i = -1.0; i <= 1.0; i += 1.0){
         c += Texel(tex, tc + i * direction);
     }
     return c / (2.0 * radius + 1.0) * color;
