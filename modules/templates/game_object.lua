01403ab86a01b831acfcaf5eaf31cf8e7d4e1517
Joao Moreira <joaomarcello.mm@gmail.com>
2024-05-22 20:45:51 -0300

    quality of life

--- modules/templates/game_object.lua
+++ modules/templates/game_object.lua
@@ -4,17 +4,18 @@ local GC = _G.JM.GameObject
 local Object = setmetatable({}, GC)
 Object.__index = Object
 
+---@return GenericObject|table
 function Object:new(x, y, w, h, draw_order, update_order, reuse_tab)
     local obj = GC:new(x, y, w, h, draw_order, update_order, reuse_tab)
     setmetatable(obj, self)
-    Object.__constructor__(obj)
-    return obj
+    return Object.__constructor__(obj)
 end
 
 function Object:__constructor__()
     --
     self.update = Object.update
     self.draw = Object.draw
+    return self
 end
 
 function Object:load()
@@ -26,15 +27,16 @@ function Object:init()
 end
 
 function Object:update(dt)
-
+    GC.update(self, dt)
 end
 
-function Object:my_draw()
+---@param self GenericObject
+local function my_draw(self)
 
 end
 
 function Object:draw()
-    GC.draw(self, self.my_draw)
+    return GC.draw(self, my_draw)
 end
 
 return Object
